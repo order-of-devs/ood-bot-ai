8bba48d6d33c4b62f62c624da1c26b871cf4ba09
Jan Banot <banot.jan@gmail.com>
2024-07-10 23:56:58 +0200

    feat: get messages command

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,11 +1,11 @@
+from datetime import datetime, timedelta
+
 import discord
 from discord.ext import commands as discord_commands
 
 from src.bot.commands.ask_ai_command import AskAICommand
 from src.bot.commands.summarize_command import SummarizeCommand
 from src.core.config import Settings
-
-OWNER_USERID = 411490244660166656
 
 
 class DiscordBot:
@@ -16,6 +16,8 @@ class DiscordBot:
         self.ask_ai_command = AskAICommand()
         self.summarize_command = SummarizeCommand()
         self.discord_token = settings.discord_token
+        self.owner_userid = settings.owner_userid
+        self.guild_id = settings.guild_id
 
     def __call__(self, *args, **kwargs):
         @self.bot.tree.command(name="ask_ai", description="ask ai command")
@@ -28,11 +30,31 @@ class DiscordBot:
             await self.summarize_command.execute(interaction.data)
             await interaction.response.send_message("Summarization started!")
 
+        @self.bot.tree.command(name="get_messages", description="get messages from a channel for the last x days")
+        async def get_messages(interaction, channel_id: str, days: int = 1):
+            if interaction.user.id == self.owner_userid:
+                channel = self.bot.get_channel(int(channel_id))
+                if not channel:
+                    await interaction.response.send_message("Channel not found.")
+                    return
+
+                end_date = datetime.utcnow()
+                start_date = end_date - timedelta(days=days)
+
+                messages = []
+                async for message in channel.history(limit=None, after=start_date, before=end_date):
+                    messages.append(f"{message.created_at}: {message.author}: {message.content}")
+
+                output_message = f"Got {len(messages)} messages from the last {days} days."
+                await interaction.response.send_message(output_message)
+            else:
+                await interaction.response.send_message("You must be the owner to use this command!")
+
         @self.bot.command()
         async def sync(ctx):
             print("sync command")
-            if ctx.author.id == OWNER_USERID:
-                guild = discord.Object(871795158608404530)
+            if ctx.author.id == self.owner_userid:
+                guild = discord.Object(self.guild_id)
                 self.bot.tree.copy_global_to(guild=guild)
                 await self.bot.tree.sync(guild=guild)
                 await self.bot.tree.sync()
--- src/core/config.py
+++ src/core/config.py
@@ -5,3 +5,5 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', extra='ignore')
 
     discord_token: str
+    owner_userid: int
+    guild_id: int
